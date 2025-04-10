301ce8e80a0fd56ffbdb5cdb3f224cea7490ec5f
Fedor Garanin <yesfedor.go@gmail.com>
2025-04-11 00:45:13 +0300

    save

--- prisma/migrations/20240623095348_update_device_v2/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `devices` MODIFY `ip` VARCHAR(32) NOT NULL;
--- prisma/migrations/20250406170711_migration_stack_v1_2/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `job_id` to the `runnable_tasks` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `runnable_tasks` ADD COLUMN `job_id` VARCHAR(128) NOT NULL;
